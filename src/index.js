46a4b823b062216b381cf8b6d34f3516c932b6d2
maxime.marc <maximemarc51@gmail.com>
2024-10-22 15:46:10 +0200

    init project

---
+++ src/index.js
@@ -0,0 +1,32 @@
+function getQueryParam(parameter) {
+  const searchParams = new URLSearchParams(window.location.search);
+  return searchParams.get(parameter);
+}
+
+function simulateTyping(element, text, delay = 100) {
+  let characterIndex = 0;
+  const intervalId = setInterval(() => {
+    if (characterIndex < text.length) {
+      element.value += text[characterIndex];
+      characterIndex++;
+    } else {
+      clearInterval(intervalId);
+      element.focus();
+    }
+  }, delay);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const mouseElement = document.getElementById("mouse");
+  const searchInputElement = document.getElementById("searchInput");
+  const queryParameter = getQueryParam("q");
+
+  if (queryParameter) {
+    mouseElement.style.display = "block";
+    mouseElement.addEventListener("animationend", () => {
+      simulateTyping(searchInputElement, queryParameter);
+    });
+  } else {
+    searchInputElement.focus();
+  }
+});
